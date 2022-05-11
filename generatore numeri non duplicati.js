e8b9fdf1f1205cf443271e20706c3f6ecc7550ff
OcimaOcram <marco.amico.pesci00@gmail.com>
2022-05-11 15:59:15 +0200

    Esercizio completo

---
+++ generatore numeri non duplicati.js
@@ -0,0 +1,29 @@
+// let array = [];
+
+// for (let i = 0; i <10; i++) {
+//     randomNumber = Math.floor(Math.random()*100 +1);
+//     array.push(randomNumber);
+    
+
+// }
+
+// console.log(array);
+
+
+//  const myArray = [];
+// while (myArray.length <10) {
+//     let randomNumber = Math.floor(Math.random()*100 +1);
+//     let trovato = myArray.includes(randomNumber);
+//     if (trovato) {
+//         console.log();
+//         myArray.push(randomNumber);
+//     }
+// }
+// console.log(myArray);
+
+const myArray = [];
+while (myArray.length < 10) {
+   let numero =prompt ("inserisci un numero")
+    myArray.push(numero);
+}
+console.log(myArray);
